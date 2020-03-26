9f57cba236171555675ffdcd8d4ab3974b89c7a3
Danila Leontyev <danila.leontyev.web@gmail.com>
2020-03-26 18:46:36 +0500

    add docker file and init nest project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import "./lib/env";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.SERVER_PORT
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+}
+bootstrap();
